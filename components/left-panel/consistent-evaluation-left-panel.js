80ef0cc78077e16b2054f1f5fe171a0521add47d
Scy Akulick <Scy.Akulick@D2L.com>
2020-07-23 11:48:54 -0400

    Added alternative display for assignments and assignment types without submissions

--- components/left-panel/consistent-evaluation-left-panel.js
+++ components/left-panel/consistent-evaluation-left-panel.js
@@ -2,6 +2,7 @@ import './consistent-evaluation-evidence.js';
 import './consistent-evaluation-submissions-page.js';
 import { css, html, LitElement } from 'lit-element';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { submissionTypesWithNoEvidence } from '../controllers/constants';
 
 export class ConsistentEvaluationLeftPanel extends LitElement {
 
@@ -24,6 +25,40 @@ export class ConsistentEvaluationLeftPanel extends LitElement {
 			d2l-consistent-evaluation-evidence {
 				overflow: hidden;
 			}
+
+			.d2l-consistent-evaluation-no-evidence {
+				align-items: center;
+				display: flex;
+				flex-direction: column;
+				height: calc(100% - 2.7rem);
+				justify-content: center;
+				margin: 0 1rem;
+				text-align: center;
+			}
+
+			.consistent-evaluation-no-evidence-image {
+				width: 100%;
+			}
+
+			.d2l-consistent-evaluation-no-submissions-container {
+				background: white;
+				border-radius: 0.3rem;
+				border: 1px solid var(--d2l-color-gypsum);
+				box-sizing: border-box;
+				margin: 1rem;
+				padding: 1rem;
+				width: 100%;
+			}
+
+			.d2l-consistent-evaluation-no-submissions {
+				background: var(--d2l-color-regolith);
+				border-radius: 0.3rem;
+				border: 1px solid var(--d2l-color-gypsum);
+				box-sizing: border-box;
+				padding: 2rem;
+				width: 100%;
+			}
+
 			d2l-consistent-evaluation-submissions-page {
 				width: 100%;
 			}
@@ -57,6 +92,23 @@ export class ConsistentEvaluationLeftPanel extends LitElement {
 	}
 
 	render() {
+		const noEvidence = 'There is no evidence uploaded for this submission type';
+		const noSubmissions = 'No submissions made for this assignment';
+		if(this.submissionInfo.submissionList == undefined) {
+			return html`${submissionTypesWithNoEvidence.includes(this.submissionInfo.submissionType) ?
+				html`
+				<div class="d2l-consistent-evaluation-no-evidence">
+					<img class="consistent-evaluation-no-evidence-image" src="https://www.clker.com/cliparts/T/B/q/X/i/r/moose.svg"></img>
+					<h1>${this.submissionInfo.submissionType}</h1>
+					<p>${noEvidence}</p>
+				</div>` :
+				html`
+				<div class="d2l-consistent-evaluation-no-submissions-container">
+					<div class="d2l-consistent-evaluation-no-submissions">${noSubmissions}</div>
+				</div>`
+			}`;
+		}
+
 		if (this._evidenceUrl) {
 			return html`
 			<d2l-consistent-evaluation-evidence
