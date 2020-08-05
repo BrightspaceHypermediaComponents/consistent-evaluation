dac13a5aa77b939767e58a2e960a3d06d8488c00
Viktor Yakovlyev <viktor.y@d2l.com>
2020-08-05 16:26:31 -0400

    initial

--- components/right-panel/consistent-evaluation-feedback-presentational.js
+++ components/right-panel/consistent-evaluation-feedback-presentational.js
@@ -2,7 +2,10 @@ import 'd2l-activities/components/d2l-activity-editor/d2l-activity-text-editor.j
 import 'd2l-activities/components/d2l-activity-editor/d2l-activity-attachments/d2l-activity-attachments-editor.js';
 import 'd2l-activities/components/d2l-activity-editor/d2l-activity-attachments/d2l-activity-attachments-list.js';
 import './consistent-evaluation-right-panel-block';
+import 'd2l-polymer-siren-behaviors/store/entity-store.js';
+
 import { html, LitElement } from 'lit-element';
+import { AttachmentCollectionEntity } from 'siren-sdk/src/activities/AttachmentCollectionEntity.js';
 import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
 import { loadLocalizationResources } from '../locale.js';
 import { LocalizeMixin } from '@brightspace-ui/core/mixins/localize-mixin.js';
@@ -63,9 +66,26 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeMixin(LitElemen
 			detail: feedback
 		}));
 	}
-	async saveAttachment() {
-		this.shadowRoot.querySelector('d2l-activity-attachments-editor').save();
+	async saveAttachment(e) {
+		const files = e.detail.files;
+		for (let i = 0; files.length > i; i++) {
+			const fileSystemType = files[i].m_fileSystemType;
+			const fileId = files[i].m_id;
+
+			const sirenEntity = await window.D2L.Siren.EntityStore.get(this.href, this.token);
+			const link = sirenEntity.getLinkByRel('https://activities.api.brightspace.com/rels/attachments');
+			const entitytemp = await window.D2L.Siren.EntityStore.fetch(link, this.token);
+
+			const entity = new AttachmentCollectionEntity(entitytemp.entity, this.token, { remove: () => { } });
+			await entity.addFileAttachment(fileSystemType, fileId);
+		}
+
 	}
+
+	async deleteAttachment() {
+		await this.shadowRoot.querySelector('d2l-activity-attachments-editor').save();
+	}
+
 	render() {
 		if (this.href && this.token) {
 
@@ -86,7 +106,7 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeMixin(LitElemen
 						@d2l-activity-attachments-picker-files-uploaded="${this.saveAttachment}"
 						@d2l-activity-attachments-picker-video-uploaded="${this.saveAttachment}"
 						@d2l-activity-attachments-picker-audio-uploaded="${this.saveAttachment}"
-						@d2l-attachment-removed="${this.saveAttachment}">
+						@d2l-attachment-removed="${this.deleteAttachment}">
 					</d2l-activity-attachments-editor>
 				</div>
 			</d2l-consistent-evaluation-right-panel-block>
