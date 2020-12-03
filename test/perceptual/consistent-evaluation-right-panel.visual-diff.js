9368e1a769acb33364dc509409dd465d4806a4e8
Scy Akulick <Scy.Akulick@D2L.com>
2020-12-03 10:23:56 -0500

    Refactor vdiff and fix outcomes

--- test/perceptual/consistent-evaluation-right-panel.visual-diff.js
+++ test/perceptual/consistent-evaluation-right-panel.visual-diff.js
@@ -1,64 +1,53 @@
 const puppeteer = require('puppeteer');
 const VisualDiff = require('@brightspace-ui/visual-diff');
 
-describe('d2l-consistent-evaluation', () => {
+describe.only('d2l-consistent-evaluation-right-panel', () => {
 
 	const visualDiff = new VisualDiff('consistent-evaluation-right-panel', __dirname);
 
 	let browser, page;
 
-	before(async() => {
+	let tests = [
+		'renders-right-panel',
+		'rubric-read-only',
+		'rich-text-editor-disabled',
+		'hiding-rubric',
+		'hiding-grade',
+		'hiding-feedback',
+		'hiding-outcomes',
+		'hiding-all'
+	];
+
+	let categories = [
+		{ name: 'desktop', width: 800 },
+		{ name: 'mobile', width: 700 }
+	];
+
+	async function setupForSize(width) {
 		browser = await puppeteer.launch({
 			headless: true,
 			args: ['--no-sandbox', '--disable-setuid-sandbox', '--lang=en-GB']
 		});
 		page = await browser.newPage();
-		await page.setViewport({width: 900, height: 900, deviceScaleFactor: 2});
+		await page.setViewport({width, height: 1200, deviceScaleFactor: 2});
 		await page.goto(`${visualDiff.getBaseUrl()}/test/perceptual/consistent-evaluation-right-panel.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
 		await page.bringToFront();
 		await visualDiff.disableAnimations(page);
-	});
+	}
 
-	after(async() => await browser.close());
+	categories.forEach((cat) => {
+		describe(cat.name, () => {
 
-	it.skip('renders the right panel', async function() {
-		const rect = await visualDiff.getRect(page, '#default');
-		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-	});
+			before(async() => setupForSize(cat.width));
+			after(async() => await browser.close());
 
-	it.skip('rubric-read-only', async function() {
-		const rect = await visualDiff.getRect(page, '#rubric-read-only');
-		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-	});
-
-	it.skip('rich-text-editor-disabled', async function() {
-		const rect = await visualDiff.getRect(page, '#rich-text-editor-disabled');
-		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-	});
-
-	it.skip('hiding-rubric', async function() {
-		const rect = await visualDiff.getRect(page, '#hiding-rubric');
-		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-	});
-
-	it.skip('hiding-grade', async function() {
-		const rect = await visualDiff.getRect(page, '#hiding-grade');
-		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-	});
-
-	it.skip('hiding-feedback', async function() {
-		const rect = await visualDiff.getRect(page, '#hiding-feedback');
-		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-	});
-
-	it.skip('hiding-outcomes', async function() {
-		const rect = await visualDiff.getRect(page, '#hiding-outcomes');
-		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
-	});
-
-	it.skip('hiding-all', async function() {
-		const rect = await visualDiff.getRect(page, '#hiding-all');
-		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+			tests.forEach((testName) => {
+				it(`${testName}`, async function() {
+					const rect = await visualDiff.getRect(page, `#${testName}`);
+					await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+				});
+			});
+		});
 	});
 
 });
