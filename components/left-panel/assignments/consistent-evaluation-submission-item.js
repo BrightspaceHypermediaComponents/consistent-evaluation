aa6c109c43af1ef99f82e293c8d98ee1a44b3726
Jamie Murray <Jamie.Murray@D2L.com>
2021-05-31 15:36:11 -0400

    DE43671 - Opening download in blank window to avoid triggering the onbeforeunload callback

--- components/left-panel/assignments/consistent-evaluation-submission-item.js
+++ components/left-panel/assignments/consistent-evaluation-submission-item.js
@@ -249,7 +249,7 @@ export class ConsistentEvaluationSubmissionItem extends RtlMixin(LocalizeConsist
 		});
 		this.dispatchEvent(event);
 
-		window.location = downloadHref;
+		window.open(downloadHref, "_blank");
 	}
 	_dispatchFileSelectedEvent(fileId) {
 		this.dispatchEvent(new CustomEvent('d2l-consistent-evaluation-file-selected', {
