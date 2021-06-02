06674caf63537642552c41dec82bdc36541db2f9
Viktor Yakovlyev <viktor.y@d2l.com>
2021-06-02 11:37:48 -0400

    Revert "appease linter gods"
    This reverts commit 6d38f41c380fd7280fc9e2c453ecaa79fb424d18.

--- test/consistent-evaluation.test.js
+++ test/consistent-evaluation.test.js
@@ -4,6 +4,7 @@ import { expect, fixture, html } from '@open-wc/testing';
 describe('d2l-consistent-evaluation', () => {
 	this.timeout(45000);
 
+
 	it('should pass all axe tests', async() => {
 		const el = await fixture(html`<d2l-consistent-evaluation></d2l-consistent-evaluation>`);
 		expect(el).to.be.accessible();
