8f925b5b7447fc9254b3864461190d51f81ac086
Scy Akulick <Scy.Akulick@D2L.com>
2021-06-14 14:46:08 -0400

    ✅ vdiff for lcb w/ profile card Co-authored-by: Viktor Yakovlyev <vix633@users.noreply.github.com>

--- test/perceptual/consistent-evaluation-lcb.visual-diff.js
+++ test/perceptual/consistent-evaluation-lcb.visual-diff.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-console */
 const puppeteer = require('puppeteer');
 const VisualDiff = require('@brightspace-ui/visual-diff');
 
-describe.skip('d2l-consistent-evaluation', () => {
-//SKIPPED SO WE CAN GET PEGGY TESTING THIS, ACTIVELY FIXING
+describe('d2l-consistent-evaluation', () => {
 	const visualDiff = new VisualDiff('consistent-evaluation-learner-context-bar', __dirname);
 
 	let browser, page;
@@ -14,12 +12,6 @@ describe.skip('d2l-consistent-evaluation', () => {
 			args: ['--no-sandbox', '--disable-setuid-sandbox', '--lang=en-GB']
 		});
 		page = await visualDiff.createPage(browser, { viewport: { width: 1000, height: 1000 } });
-		page
-			.on('console', message =>
-				console.log(`${message.type().substr(0, 3).toUpperCase()} ${message.text()}`))
-			.on('pageerror', ({ message }) => console.log(message))
-			.on('requestfailed', request =>
-				console.log(`${request.failure().errorText} ${request.url()}`));
 		await page.goto(`${visualDiff.getBaseUrl()}/test/perceptual/consistent-evaluation-lcb.visual-diff.html`, { waitUntil: ['networkidle0', 'load'] });
 		await page.bringToFront();
 	});
@@ -37,12 +29,9 @@ describe.skip('d2l-consistent-evaluation', () => {
 	});
 
 	it('renders learner profile card', async function() {
-		await page.$eval('#default', async(elem) => {
+		await page.$eval('#open-card', async(elem) => {
 			const lcb = elem.querySelector('d2l-consistent-evaluation-learner-context-bar');
-			const userContext = lcb.querySelector('d2l-consistent-evaluation-lcb-user-context');
-			console.log('==========================================================');
-			console.log(JSON.stringify(userContext));
-			console.log('==========================================================');
+			const userContext = lcb.shadowRoot.querySelector('d2l-consistent-evaluation-lcb-user-context');
 			const card = userContext.shadowRoot.querySelector('d2l-labs-user-profile-card');
 			card._userProfileCardSettings = {
 				showPicture: true,
@@ -60,7 +49,7 @@ describe.skip('d2l-consistent-evaluation', () => {
 			card.open();
 			return listener;
 		});
-		const rect = await visualDiff.getRect(page, '#default');
+		const rect = await visualDiff.getRect(page, '#open-card-wrapper');
 		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
 	});
 
