a752d624cbaa488783b8bcd28c704e2e12c8c436
Scy Akulick <50635849+Scy-D2L@users.noreply.github.com>
2021-06-23 16:15:00 -0400

    Scy/us128856/ratings screen readers (#563)
    * ♿️ Summarize ratings information for screenreaders
    * 🌐 Improve / make consistent rubrics pluralization
    * Updating Visual Diff Goldens (#564)

--- components/left-panel/discussions/consistent-evaluation-discussion-post-rating.js
+++ components/left-panel/discussions/consistent-evaluation-discussion-post-rating.js
@@ -1,9 +1,9 @@
+import '@brightspace-ui/core/components/offscreen/offscreen.js';
 import { css, html, LitElement } from 'lit-element';
 import { fivestarRatingClass, upvoteDownvoteRatingClass, upvoteOnlyRatingClass } from '../../controllers/constants.js';
 import { bodyCompactStyles } from '@brightspace-ui/core/components/typography/styles.js';
 import { LocalizeConsistentEvaluation } from '../../../localize-consistent-evaluation.js';
 import { RtlMixin } from '@brightspace-ui/core/mixins/rtl-mixin.js';
-
 export class ConsistentEvaluationDiscussionPostRating extends RtlMixin(LocalizeConsistentEvaluation(LitElement)) {
 	static get properties() {
 		return {
@@ -53,8 +53,12 @@ export class ConsistentEvaluationDiscussionPostRating extends RtlMixin(LocalizeC
 	}
 
 	_renderFiveStarRating() {
+		const fiveStarRatingText = this.ratingInformation.numRatings <= 0
+			? this.localize('numRatings', 'num', 0)
+			: this.localize('fiveStarRatings', { numStars: this.ratingInformation.ratingAverage, numRatings: this.ratingInformation.numRatings });
 		return html`
-			<div class="d2l-body-compact d2l-consistent-evaluation-discussion-evidence-body-rating-text center">
+			<d2l-offscreen>${fiveStarRatingText}</d2l-offscreen>
+			<div class="d2l-body-compact d2l-consistent-evaluation-discussion-evidence-body-rating-text center" aria-hidden="true">
 				${(this.ratingInformation.numRatings <= 0) ? '-' : this.ratingInformation.ratingAverage} / 5
 				<d2l-icon icon="tier1:subscribe-filled"></d2l-icon> <br>
 				(${this.localize('numRatings', 'num', this._checkMaxRatingDisplay(this.ratingInformation.numRatings))})
@@ -64,7 +68,8 @@ export class ConsistentEvaluationDiscussionPostRating extends RtlMixin(LocalizeC
 
 	_renderUpvoteDownvote() {
 		return html`
-			<div class="d2l-body-compact d2l-consistent-evaluation-discussion-evidence-body-rating-text">
+			<d2l-offscreen>${this.localize('upVotesAndDownVotes', { numUpVotes: this.ratingInformation.numUpVotes, numDownVotes: this.ratingInformation.numDownVotes })}</d2l-offscreen>
+			<div class="d2l-body-compact d2l-consistent-evaluation-discussion-evidence-body-rating-text" aria-hidden="true">
 				${this.localize('upVotes', 'numUpVotes', this._checkMaxRatingDisplay(this.ratingInformation.numUpVotes))}<br>
 				${this.localize('downVotes', 'numDownVotes', this._checkMaxRatingDisplay(this.ratingInformation.numDownVotes))}
 			</div>
