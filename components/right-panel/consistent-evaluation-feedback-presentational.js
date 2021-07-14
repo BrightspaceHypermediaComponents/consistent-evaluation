136075bb603bb9041b9c6d2aeefa7b67cf36700e
Maddie Gabriel <Maddie.Gabriel@D2L.com>
2021-07-14 12:08:52 -0400

    more linter

--- components/right-panel/consistent-evaluation-feedback-presentational.js
+++ components/right-panel/consistent-evaluation-feedback-presentational.js
@@ -9,8 +9,8 @@ import '@brightspace-ui/core/components/list/list-item.js';
 import '@brightspace-ui/core/components/list/list-item-content.js';
 import { css, html, LitElement } from 'lit-element';
 import { convertToken } from '../helpers/converterHelpers.js';
-import { deleteCommentActionName } from '../controllers/constants.js';
 import { Debouncer } from '@polymer/polymer/lib/utils/debounce.js';
+import { deleteCommentActionName } from '../controllers/constants.js';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
 import { LocalizeConsistentEvaluation } from '../../localize-consistent-evaluation.js';
 import { radioStyles } from '@brightspace-ui/core/components/inputs/input-radio-styles.js';
@@ -86,22 +86,6 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeConsistentEvalu
 		};
 	}
 
-	constructor() {
-		super();
-		this.commentBankOption = 0;
-		this.canEditFeedback = false;
-		this.canAddFile = false;
-		this.canRecordVideo = false;
-		this.canRecordAudio = false;
-		this.canAddFeedbackLink = false;
-		this.canAddFeedbackGoogleDriveLink = false;
-		this.canAddFeedbackOneDriveLink = false;
-		this.useNewHtmlEditor = false;
-		this.commentBankOpen = false;
-		this._debounceJobs = {};
-		this.flush = this.flush.bind(this);
-	}
-
 	static get styles() {
 		return [radioStyles, css`
 			#comment-bank-button {
@@ -122,6 +106,23 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeConsistentEvalu
 			}
 		`];
 	}
+
+	constructor() {
+		super();
+		this.commentBankOption = 0;
+		this.canEditFeedback = false;
+		this.canAddFile = false;
+		this.canRecordVideo = false;
+		this.canRecordAudio = false;
+		this.canAddFeedbackLink = false;
+		this.canAddFeedbackGoogleDriveLink = false;
+		this.canAddFeedbackOneDriveLink = false;
+		this.useNewHtmlEditor = false;
+		this.commentBankOpen = false;
+		this._debounceJobs = {};
+		this.flush = this.flush.bind(this);
+	}
+
 	connectedCallback() {
 		super.connectedCallback();
 		this.addEventListener('d2l-request-provider', this.htmlEditorEnabled);
@@ -189,13 +190,27 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeConsistentEvalu
 		}
 	}
 
-	_resizeDialog() {
-		const dialog = this.shadowRoot.querySelector('d2l-consistent-evaluation-right-panel-block').querySelector('d2l-dialog');
-		dialog.resize();
+	_addFeedbackFromBank(text) {
+		this.feedbackText += ' ' + text;
+		this.commentBankOpen = false;
+	}
+
+	_deleteFeedbackFromBank(commentId) {
+		this._emitDeleteSavedFeedbackEvent(commentId);
+	}
+
+	_emitDeleteSavedFeedbackEvent(commentId) {
+		this.dispatchEvent(new CustomEvent('on-d2l-consistent-evaluation-delete-saved-feedback', {
+			composed: true,
+			bubbles: true,
+			detail: {
+				commentIdToDelete: commentId
+			}
+		}));
 	}
 
 	_emitFeedbackEditEvent(feedback) {
-		let commentBankOption = this.commentBankOption;
+		const commentBankOption = this.commentBankOption;
 		this.dispatchEvent(new CustomEvent('on-d2l-consistent-eval-feedback-edit', {
 			composed: true,
 			bubbles: true,
@@ -205,12 +220,36 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeConsistentEvalu
 			}
 		}));
 	}
+
 	_emitFeedbackTextEditorChangeEvent() {
 		this.dispatchEvent(new CustomEvent('on-d2l-consistent-eval-feedback-text-editor-change', {
 			composed: true,
 			bubbles: true
 		}));
 	}
+
+	_emitGetMySavedFeedbackEvent(search) {
+		this.dispatchEvent(new CustomEvent('on-d2l-consistent-evaluation-get-my-saved-feedback', {
+			composed: true,
+			bubbles: true,
+			detail: search
+		}));
+	}
+
+	_getCommentBank() {
+		return html`
+			<d2l-dialog
+				title-text='Comment BANK!'
+				?opened=${this.commentBankOpen}
+				width=1000
+				id="comment-bank-dialog"
+				@d2l-dialog-close=${this._onCommentBankClose}>
+					${this._renderCommentBankSearch()}
+					${this._renderCommentBank()}
+			</d2l-dialog>
+		`;
+	}
+
 	_getHtmlEditor() {
 		if (this.useNewHtmlEditor) {
 			import('@brightspace-ui/htmleditor/htmleditor.js');
@@ -238,58 +277,6 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeConsistentEvalu
 		}
 	}
 
-	_getCommentBank() {
-		return html`
-			<d2l-dialog
-				title-text='Comment BANK!'
-				?opened=${this.commentBankOpen}
-				width=1000
-				id="comment-bank-dialog"
-				@d2l-dialog-close=${this._onCommentBankClose}>
-					${this._renderCommentBankSearch()}
-					${this._renderCommentBank()}
-			</d2l-dialog>
-		`;
-	}
-
-	_renderCommentBankSearch() {
-		return html`
-			<d2l-input-search
-				label="Search"
-				placeholder="Search comments"
-				@d2l-input-search-searched=${this._onCommentBankSearch}>
-			</d2l-input-search>
-			<div class="d2l-consistent-evaluation-right-panel-clearfix"></div>
-		`;
-	}
-
-	_renderCommentBankOptions() {
-		return html`
-			<label class="d2l-input-radio-label">
-				<input type="radio" name="commentBankGroup" checked value=0  @change="${this._onCommentBankOptionChange}">
-				Do not store comment
-			</label>
-			<label class="d2l-input-radio-label">
-				<input type="radio" name="commentBankGroup" value=1 @change="${this._onCommentBankOptionChange}">
-				Store comment in personal bank
-			</label>
-			<label class="d2l-input-radio-label">
-				<input type="radio" name="commentBankGroup" value=2 @change="${this._onCommentBankOptionChange}">
-				Store comment in public bank
-			</label>
-		`;
-	  }
-
-	_saveOnFeedbackChange(e) {
-		const feedback = e.detail.content;
-		this._emitFeedbackTextEditorChangeEvent();
-		this._debounceJobs.feedback = Debouncer.debounce(
-			this._debounceJobs.feedback,
-			timeOut.after(800),
-			() => this._emitFeedbackEditEvent(feedback)
-		);
-	}
-
 	_onCommentBankClose() {
 		this.commentBankOpen = false;
 	}
@@ -299,22 +286,8 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeConsistentEvalu
 		this.commentBankOpen = true;
 	}
 
-	_emitGetMySavedFeedbackEvent(search) {
-		this.dispatchEvent(new CustomEvent('on-d2l-consistent-evaluation-get-my-saved-feedback', {
-			composed: true,
-			bubbles: true,
-			detail: search
-		}));
-	}
-
-	_emitDeleteSavedFeedbackEvent(commentId) {
-		this.dispatchEvent(new CustomEvent('on-d2l-consistent-evaluation-delete-saved-feedback', {
-			composed: true,
-			bubbles: true,
-			detail: {
-				commentIdToDelete: commentId
-			}
-		}));
+	_onCommentBankOptionChange(e) {
+		this.commentBankOption = e.target.value;
 	}
 
 	_onCommentBankSearch(e) {
@@ -322,13 +295,9 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeConsistentEvalu
 		this._emitGetMySavedFeedbackEvent(searchTerm);
 	}
 
-	_onCommentBankOptionChange(e) {
-		this.commentBankOption = e.target.value
-	}
-
 	_renderCommentBank() {
-		if(!this.mySavedFeedback){
-			return html``
+		if (!this.mySavedFeedback) {
+			return html``;
 		}
 
 		if (this.mySavedFeedback.length === 0) {
@@ -344,7 +313,7 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeConsistentEvalu
 		}
 
 		const itemTemplate = this.mySavedFeedback.map(feedback => {
-			let onClickHandler = () => this._addFeedbackFromBank(feedback.properties.text);
+			const onClickHandler = () => this._addFeedbackFromBank(feedback.properties.text);
 
 			return html`
 				<d2l-list-item>
@@ -364,7 +333,7 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeConsistentEvalu
 				</d2l-list-item>
 			`;
 		});
-                        
+
 		return html`
 			<d2l-list separators="between">
 				${itemTemplate}
@@ -373,29 +342,62 @@ class ConsistentEvaluationFeedbackPresentational extends LocalizeConsistentEvalu
 		`;
 	}
 
+	_renderCommentBankOptions() {
+		return html`
+			<label class="d2l-input-radio-label">
+				<input type="radio" name="commentBankGroup" checked value=0  @change="${this._onCommentBankOptionChange}">
+				Do not store comment
+			</label>
+			<label class="d2l-input-radio-label">
+				<input type="radio" name="commentBankGroup" value=1 @change="${this._onCommentBankOptionChange}">
+				Store comment in personal bank
+			</label>
+			<label class="d2l-input-radio-label">
+				<input type="radio" name="commentBankGroup" value=2 @change="${this._onCommentBankOptionChange}">
+				Store comment in public bank
+			</label>
+		`;
+	}
+
+	_renderCommentBankSearch() {
+		return html`
+			<d2l-input-search
+				label="Search"
+				placeholder="Search comments"
+				@d2l-input-search-searched=${this._onCommentBankSearch}>
+			</d2l-input-search>
+			<div class="d2l-consistent-evaluation-right-panel-clearfix"></div>
+		`;
+	}
+
 	_renderDeleteButton(feedback) {
 		if (feedback.hasActionByName(deleteCommentActionName)) {
-			let onDeleteHandler = () => this._deleteFeedbackFromBank(feedback.properties.commentId);
+			const onDeleteHandler = () => this._deleteFeedbackFromBank(feedback.properties.commentId);
 			return html`
 				<d2l-button-icon
-					id="delete-comment-button"
-					text="Delete comment"
-					icon="tier1:delete"
+					id='delete-comment-button'
+					text='Delete comment'
+					icon='tier1:delete'
 					@click=${onDeleteHandler}
 				></d2l-button-icon>
 			`;
 		}
-		return ``;
+		return html``;
 	}
 
-	_addFeedbackFromBank(text) {
-		this.feedbackText += ' ' + text;
-		this.commentBankOpen = false;
+	_resizeDialog() {
+		const dialog = this.shadowRoot.querySelector('d2l-consistent-evaluation-right-panel-block').querySelector('d2l-dialog');
+		dialog.resize();
 	}
 
-	_deleteFeedbackFromBank(commentId) {
-		this._emitDeleteSavedFeedbackEvent(commentId);
-		console.log(this.mySavedFeedback)
+	_saveOnFeedbackChange(e) {
+		const feedback = e.detail.content;
+		this._emitFeedbackTextEditorChangeEvent();
+		this._debounceJobs.feedback = Debouncer.debounce(
+			this._debounceJobs.feedback,
+			timeOut.after(800),
+			() => this._emitFeedbackEditEvent(feedback)
+		);
 	}
 
 	_saveOnFeedbackChangeNewEditor() {
